9cae6f89815c8d7e0abfa7e43e321cc7007e2479
Jewbacca <bensousan.william@gmail.com>
2017-01-06 11:22:18 +0100

    -_-

--- src/Game2048.java
+++ src/Game2048.java
@@ -261,7 +261,7 @@ public class Game2048 extends JPanel {
     int xOffset = offsetCoors(x);
     int yOffset = offsetCoors(y);
     g.setColor(tile.getBackground());
-    g.fillRoundRect(xOffset, yOffset, TILE_SIZE, TILE_SIZE, 14, 14);
+    g.fillRoundRect(xOffset, yOffset, TILE_SIZE, TILE_SIZE, 0, 0);
     g.setColor(tile.getForeground());
     final int size = value < 100 ? 36 : value < 1000 ? 32 : 24;
     final Font font = new Font(FONT_NAME, Font.BOLD, size);
@@ -301,43 +301,6 @@ public class Game2048 extends JPanel {
 
   private static int offsetCoors(int arg) {
     return arg * (TILES_MARGIN + TILE_SIZE) + TILES_MARGIN;
-  }
-
-  static class Tile {
-    int value;
-
-    public Tile() {
-      this(0);
-    }
-
-    public Tile(int num) {
-      value = num;
-    }
-
-    public boolean isEmpty() {
-      return value == 0;
-    }
-
-    public Color getForeground() {
-      return value < 16 ? new Color(0x776e65) :  new Color(0xf9f6f2);
-    }
-
-    public Color getBackground() {
-      switch (value) {
-        case 2:    return new Color(0xeee4da);
-        case 4:    return new Color(0xede0c8);
-        case 8:    return new Color(0xf2b179);
-        case 16:   return new Color(0xf59563);
-        case 32:   return new Color(0xf67c5f);
-        case 64:   return new Color(0xf65e3b);
-        case 128:  return new Color(0xedcf72);
-        case 256:  return new Color(0xedcc61);
-        case 512:  return new Color(0xedc850);
-        case 1024: return new Color(0xedc53f);
-        case 2048: return new Color(0xedc22e);
-      }
-      return new Color(0xcdc1b4);
-    }
   }
 
   public static void main(String[] args) {
